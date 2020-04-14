fcd98a1bade4b7a0b8f42274a1099cb695dda99a
murali-ams <murali-ams@support-meraas.ae>
2020-04-14 17:28:13 +0400

    adding client changes.

---
+++ build/precache-manifest.3b91557f6d0dd467df7dc6411e9ac167.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4e2b516f3b02bedf847a21d9d7b53be7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b811d02edf66c5e4a726",
+    "url": "/static/css/main.a55608f4.chunk.css"
+  },
+  {
+    "revision": "6032ea5fc2c75a22bfd1",
+    "url": "/static/js/2.fd14103c.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.fd14103c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b811d02edf66c5e4a726",
+    "url": "/static/js/main.bffae226.chunk.js"
+  },
+  {
+    "revision": "50e5e7aff7812c395c01",
+    "url": "/static/js/runtime-main.7e837300.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
